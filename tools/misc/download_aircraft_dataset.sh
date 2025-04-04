41ece7859c2091311749f23b65983c2449135c3b
Gogonemnem <glau.github@pm.me>
2025-04-04 12:37:08 +0200

    remove directory check

--- tools/misc/download_aircraft_dataset.sh
+++ tools/misc/download_aircraft_dataset.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-# make sure that directory exists if not then create it
-mkdir -p ./data/aircrafts
-
-# download the dataset from kaggle
 kaggle datasets download a2015003713/militaryaircraftdetectiondataset -p ./data/aircraft --unzip
