2bfc7ea4b0f5c3071cd478fbfed2c64429907a83
Gogonemnem <glau.github@pm.me>
2025-05-08 12:33:45 +0200

    use balanced hierarchical loss, and reuse code

--- hod/models/losses/hierarchical_focal_loss.py
+++ hod/models/losses/hierarchical_focal_loss.py
@@ -1,198 +1,135 @@
 import torch
-import torch.nn.functional as F
+
 from mmengine.fileio import load
 from mmdet.registry import MODELS
-from mmdet.models.losses.focal_loss import FocalCustomLoss
-from mmdet.models.losses.utils import weight_reduce_loss
+from mmdet.models.losses.focal_loss import FocalLoss
 from mmdet.models.losses.focal_loss import py_sigmoid_focal_loss
 
 from hod.utils.tree import HierarchyTree
 
 @MODELS.register_module()
-class HierarchicalFocalLoss(FocalCustomLoss):
+class HierarchicalFocalLoss(FocalLoss):
     def __init__(self,
                  ann_file='',
-                 **ce_kwargs):
+                 decay=1,
+                 **kwargs):
         """
-        ce_kwargs are forwarded to CrossEntropyCustomLoss,
+        kwargs are forwarded to FocalLoss,
         e.g. `num_classes=len(class_to_idx)`, `ignore_index=...`, etc.
         """
-        super().__init__(**ce_kwargs)
+        super().__init__(**kwargs)
         # we'll keep a little cache of each parent-child index list
-        self._siblings_cache = {}
+        self.decay = decay
         self.load_taxonomy(ann_file)
-    
+
     def load_taxonomy(self, ann_file):
         ann = load(ann_file)
         cats = ann['categories']
-        taxonomy = ann.get('taxonomy', {})
-
         # build name<->idx map
         self.class_to_idx = {c['name']: c['id'] for c in cats}
-        
-        if not self.use_sigmoid:
-            bg_id = len(self.class_to_idx)
-            self.class_to_idx['background'] = bg_id
-            taxonomy['background'] = {}    # make background a child of root
-            taxonomy = {'root': taxonomy}
 
         # build the tree
+        taxonomy = ann.get('taxonomy', {})
         self.tree = HierarchyTree(taxonomy)
 
-        # NOW precompute the “siblings” groups and their level‐weights
         device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-        # preallocate the dt_paths table
-        C = len(self.class_to_idx)
-        self.dt_path_mask = torch.zeros((C+1, C),
-                                   dtype=torch.bool,
-                                   device=device)
-        self.class_depth = torch.zeros(C, device=device)
-
-        self.siblings_idxs = []    # List[LongTensor] each = the column‐indices of one siblings‐set
-        self.level_weights = []    # List[float]    each = exp(-alpha * depth_of_parent)
-        self.kids = []
-
-        # walk all nodes; group by “the parent” (skip root itself)
-        for pname, parent in self.tree.class_to_node.items():
-            # 1) build paths for *this* node
-            # get_path returns a list of ids from root→...→pname
-            this_idx = self.class_to_idx[pname]
-            path = self.tree.get_path(pname)   # e.g. [0, 3, 17]
-            self.class_depth[this_idx] = len(path) - 1
-            for p in path:
-                j = self.class_to_idx[p]
-                self.dt_path_mask[this_idx, j] = True
-
-            # 2) build sibling‐set if this node has children
-            kids = parent.children
-            if not kids:
-                continue            # skip leaves
-
-            # make one LongTensor of all the kids’ global idxs
-            idxs = torch.tensor(
-                [self.class_to_idx[ch.name] for ch in kids],
-                device=device,
-                dtype=torch.long
-            )
-            self.siblings_idxs.append(idxs)
-            self.kids.append(kids)
-
-            # depth_of_parent = #ancestors of pname
-            depth = self.tree.get_depth(pname) + 1 # depth starts at zero
-            w = torch.exp(-self.alpha * depth * torch.ones((), device=device))
-            self.level_weights.append(w)
-
-        max_depth = self.class_depth.max()  # Find the maximum depth
-        class_level_weight = torch.exp(-self.alpha * (max_depth - self.class_depth))  # shape (C,)
-        # class_level_weight = torch.exp(-self.alpha * self.class_depth)  # shape (C,)
-        # class_level_weight = torch.ones_like(self.class_depth) # shape (C,)
-
-        depths_for_norm = torch.arange(max_depth + 1, device=device)
-        norm = torch.exp(-self.alpha * depths_for_norm).sum()
-        # norm = max_depth
-
-        # Normalize weights, handling potential division by zero or very small norm
-        if norm > 1e-6:
-            self.class_level_weight = class_level_weight / norm
-        else:
-            self.class_level_weight = class_level_weight
-
-    def forward(self, cls_score, labels, weight, **kwargs):
-        """
-        cls_score: (Batch * Queries, C+1) logits over all fine-grained classes + background
-        labels:    (Batch * Queries) each in [0..C-1] or =ignore_index
-        """
-        if self.use_sigmoid:
-            return self.forward_sigmoid(cls_score, labels, weight, **kwargs)
-        else:
-            return self.forward_softmax(cls_score, labels, weight, **kwargs)
-
-    def forward_sigmoid(self, logits, labels, weight=None, **kwargs):
-        # cls_labels is an (M,) LongTensor of the ground‐truth labels in [0..C−1]`
-        # Multilabel
-        targets = self.dt_path_mask[labels]     # (M, C), bool
-        
-        # Single label
-        # targets = F.one_hot(labs, num_classes=C+1)  # (M, C+1), bool
-        # targets = targets[:, :C]
-        # targets = targets.float()            # convert to float
-
-        losses = py_sigmoid_focal_loss(
-            logits,
-            targets.float(),
-            weight=None,
-            gamma=self.gamma,
-            alpha=self.alpha,
-            reduction='none',
-            avg_factor=None,
+        C = len(cats)
+        self.ancestor_path_target_mask = torch.zeros(
+            (C+1, C),
+            dtype=torch.bool,
+            device=device
         )
-        
-        weighted_loss = losses * self.class_level_weight  # (M,C) broadcast
-        if weight is not None:
-            if weight.shape != weighted_loss.shape:
-                if weight.size(0) == weighted_loss.size(0):
-                    # For most cases, weight is of shape (num_priors, ),
-                    #  which means it does not have the second axis num_class
-                    weight = weight.view(-1, 1)
-                else:
-                    # Sometimes, weight per anchor per class is also needed. e.g.
-                    #  in FSAF. But it may be flattened of shape
-                    #  (num_priors x num_class, ), while loss is still of shape
-                    #  (num_priors, num_class).
-                    assert weight.numel() == weighted_loss.numel()
-                    weight = weight.view(weighted_loss.size(0), -1)
-            assert weight.ndim == weighted_loss.ndim
-        loss = weight_reduce_loss(weighted_loss, weight=weight, reduction='mean', avg_factor=kwargs.get('avg_factor'))
-        return self.loss_weight * loss
-    
-    def forward_softmax(self, cls_score, labels, weight, **kwargs):
-        N, Cplus1 = cls_score.shape
-        device = cls_score.device
+        self.class_level_weight = torch.zeros((C,), device=device)
 
-        # drop ignored and background‐labels in one go
-        if self.ignore_index is None or self.ignore_index < 0:
-            valid = torch.ones_like(labels, dtype=torch.bool)
-        else:
-            valid = (labels != self.ignore_index)
-        labs  = labels[valid]                   # (N_valid,)
-        logits = cls_score[valid]               # (N_valid, Cplus1)
-        logp = logits.log_softmax(dim=1).exp() # global probs
+        # differs from paper: path_weight = 1/2
+        total_path_weight  = 1.0 # changing it equals changing loss_weight
 
-        total_loss   = torch.zeros((), device=device)
-        total_w = torch.zeros((), device=device)
-
-        # we also want a global2local map per siblings‐set,
-        # but we can build that on the fly from idxs.
-        for (idxs, w) in zip(self.siblings_idxs, self.level_weights):
-            # gather only the relevant columns (the “siblings”)
-            local_logits = logp[:, idxs]       # (N_valid, S)
-
-            # make a global->local index map for this set
-            g2l = -torch.ones(Cplus1, device=device, dtype=torch.long)
-            g2l[idxs] = torch.arange(len(idxs), device=device)
-
-            # map the lab indices
-            target = g2l[labs]                   # (N_valid,) each in [0..S-1]
-
-            # After you build g2l and target = g2l[labs]:
-            in_group = target >= 0           # a boolean mask of shape (N_valid,)
-            in_group = target >= 0
-            if in_group.any():
-                # real group
-                lg = local_logits[in_group]
-                tg = target[in_group]
-            else:
+        for cls, node in self.tree.class_to_node.items():
+            if cls not in self.class_to_idx:
                 continue
-                # dummy pass so DDP sees these params in the graph
-                # lg = local_logits[:1]             # pick any row
-                # tg = torch.zeros(1, dtype=torch.long, device=device)
-                # w = 0  # zero it out
+            idx = self.class_to_idx[cls]
+            # get the path from the root
+            path_cls = self.tree.get_path(cls)
+            path_idx = [self.class_to_idx[p] for p in path_cls]
 
-            loss_l = F.cross_entropy(lg, tg, reduction=self.reduction)
-            total_loss += w * loss_l
-            total_w    += w
+            self.ancestor_path_target_mask[idx, path_idx] = True
 
-        total_loss = total_loss / total_w
+            # Compute the node's weight contribution using exponential decay
+            height = node.get_height()
+            node_weight_contribution  = (1-self.decay)/(1-self.decay**(height+1)) if self.decay != 1 else 1/(height+1)
 
-        return self.loss_weight * total_loss
+            # differs from the original paper, root class a weight != 0
+            # we also slice the node itself but the weight is zero at init
+            ancestor_weight = self.class_level_weight[path_idx].sum()
+            available_weight = total_path_weight - ancestor_weight
+
+            # Assign the final weight to the current class
+            class_weight = node_weight_contribution  * available_weight
+            self.class_level_weight[idx] = class_weight
+
+    def forward(self,
+                pred,
+                target,
+                weight=None,
+                avg_factor=None,
+                reduction_override=None):
+        """Forward function. Very similar to the original Focal Loss forward
+        function, but with some changes to support the hierarchical loss.
+        The main difference is that we use the ancestor_path_target_mask to
+        calculate the loss for each class in the hierarchy. Also, we use the
+        class_level_weight to calculate the weight for each class in the
+        hierarchy.
+
+        Args:
+            pred (torch.Tensor): The prediction.
+            target (torch.Tensor): The learning label of the prediction.
+                The target shape support (N,C) or (N,), (N,C) means
+                one-hot form.
+            weight (torch.Tensor, optional): The weight of loss for each
+                prediction. Defaults to None.
+            avg_factor (int, optional): Average factor that is used to average
+                the loss. Defaults to None.
+            reduction_override (str, optional): The reduction method used to
+                override the original reduction method of the loss.
+                Options are "none", "mean" and "sum".
+
+        Returns:
+            torch.Tensor: The calculated loss
+        """
+        assert reduction_override in (None, 'none', 'mean', 'sum')
+        reduction = (
+            reduction_override if reduction_override else self.reduction)
+        if self.use_sigmoid:
+            target = self.ancestor_path_target_mask[target]
+            calculate_loss_func = py_sigmoid_focal_loss
+
+            if weight is not None:
+                if weight.shape != target.shape:
+                    if weight.size(0) == target.size(0):
+                        # For most cases, weight is of shape (num_priors, ),
+                        #  which means it does not have the second axis num_class
+                        weight = weight.view(-1, 1)
+                    else:
+                        # Sometimes, weight per anchor per class is also needed. e.g.
+                        #  in FSAF. But it may be flattened of shape
+                        #  (num_priors x num_class, ), while targets is still of shape
+                        #  (num_priors, num_class).
+                        assert weight.numel() == target.numel()
+                        weight = weight.view(target.size(0), -1)
+                assert weight.ndim == target.ndim
+                # weight now can be multiplied by the class level weights
+                #  to get the final weight
+                weight = weight * self.class_level_weight
+
+            loss_cls = self.loss_weight * calculate_loss_func(
+                pred,
+                target,
+                weight,
+                gamma=self.gamma,
+                alpha=self.alpha,
+                reduction=reduction,
+                avg_factor=avg_factor)
+
+        else:
+            raise NotImplementedError
+        return loss_cls
