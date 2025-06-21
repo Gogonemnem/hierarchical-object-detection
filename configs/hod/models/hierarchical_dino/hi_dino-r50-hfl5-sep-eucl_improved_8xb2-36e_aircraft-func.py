ae57708fa614119ed872852b61c0ce47922709fe
Gogonemnem <glau.github@pm.me>
2025-06-21 08:37:01 +0000

    update configs

---
+++ configs/hod/models/hierarchical_dino/hi_dino-r50-hfl5-sep-eucl_improved_8xb2-36e_aircraft-func.py
@@ -0,0 +1,32 @@
+_base_ = [
+    '../../_base_/hierarchical-dino-4scale_r50_improved.py',
+    '../../datasets/aircraft_hierarchy_function.py',
+    '../../schedules/schedule_r50_improved_8xb2-36e.py'
+]
+
+custom_imports = dict(imports=['hod.datasets', 'hod.evaluation', 'hod.models'], allow_failed_imports=False)
+
+data_root = 'data/aircraft/hierarchy_function/'
+model = dict(
+    bbox_head=dict(
+        num_classes=111,
+        loss_cls=dict(
+            ann_file=_base_.test_evaluator.ann_file,
+            decay=5,
+        )
+    ),
+    # training and testing settings
+    train_cfg=dict(
+        assigner=dict(
+            type='HungarianAssigner',
+            match_costs=[
+                dict(
+                    type='HierarchicalFocalLossCost',
+                    weight=2.0,
+                    ann_file=_base_.test_evaluator.ann_file,
+                    decay=5,
+                ),
+                dict(type='BBoxL1Cost', weight=5.0, box_format='xywh'),
+                dict(type='IoUCost', iou_mode='giou', weight=2.0)
+            ])),
+)
