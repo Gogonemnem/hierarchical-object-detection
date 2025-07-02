e67ad244f8aa41284eb4962523bda8b871f72ca2
Gogonemnem <glau.github@pm.me>
2025-07-02 13:49:36 +0200

    add exclusion experiments with entailment

---
+++ configs/hod/models/hierarchical_dino/hi_dino-r50-hfl2-ecl-sep-eucl_improved_8xb2-36e_aircraft-func-excluded-F16.Mig29.B1.C130.TB2.AH64.py
@@ -0,0 +1,36 @@
+_base_ = [
+    '../../_base_/hierarchical-dino-4scale_r50_improved.py',
+    '../../datasets/aircraft_hierarchy_function_excluded_F16.Mig29.B1.C130.TB2.AH64.py',
+    '../../schedules/schedule_r50_improved_8xb2-36e.py'
+]
+
+custom_imports = dict(imports=['hod.datasets', 'hod.evaluation', 'hod.models'], allow_failed_imports=False)
+
+model = dict(
+    bbox_head=dict(
+        num_classes=111,
+        loss_embed=dict(
+            type='EntailmentConeLoss',
+            ann_file=_base_.test_evaluator.ann_file,
+            loss_weight=1.0,
+        ),
+        loss_cls=dict(
+            ann_file=_base_.test_evaluator.ann_file,
+            decay=2,
+        )
+    ),
+    # training and testing settings
+    train_cfg=dict(
+        assigner=dict(
+            type='HungarianAssigner',
+            match_costs=[
+                dict(
+                    type='HierarchicalFocalLossCost',
+                    weight=2.0,
+                    ann_file=_base_.test_evaluator.ann_file,
+                    decay=2,
+                ),
+                dict(type='BBoxL1Cost', weight=5.0, box_format='xywh'),
+                dict(type='IoUCost', iou_mode='giou', weight=2.0)
+            ])),
+)
